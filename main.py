74bf8b05ff7f16ddbceb0ea1a3eef08184d47008
Jett Pennington <jettpennington@gmail.com>
2023-11-02 20:16:43 +0000

    added friends and enimes visable in desciption

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from connection import create_connection, execute_query, execute_modify
 
 create_connection("postgres", "postgres", "postgres")
 
-# query = """
-# SELECT * from heroes
-# """
-# print(execute_query(query))
-
 def view_hero():
     query = """
     SELECT name, id FROM heroes
@@ -27,14 +22,18 @@ def view_hero():
     """.format(hero_id)
     hero_data = execute_query(query)
     hero_powers = convert_hero_id_to_powers(hero_id)
+    [hero_friends, hero_enemies] = convert_hero_id_to_relationships(hero_id)
     hero_info = """
     Hero Name: {}
     Hero About Me: {}
 
     {}'s Power(s): {}
 
+    Friends: {}
+    Enemies: {}
+
     Biography: {}
-    """.format(hero_data[0][1], hero_data[0][2], hero_data[0][1], hero_powers, hero_data[0][3])
+    """.format(hero_data[0][1], hero_data[0][2], hero_data[0][1], hero_powers, hero_friends, hero_enemies, hero_data[0][3])
     print(hero_info)
 
 def convert_hero_id_to_powers(hero_id):
@@ -53,9 +52,38 @@ def convert_hero_id_to_powers(hero_id):
         out.append(execute_query(query)[0][0])
     return ", ".join(out)
 
+def convert_hero_id_to_relationships(hero_id):
+    query = """
+    SELECT hero2_id, relationship_type_id FROM relationships WHERE hero1_id = {}
+    """.format(hero_id)
+    hero_relationships = execute_query(query)
+
+    friends = []
+    enemies = []
+    
+    for item in hero_relationships:
+        query = """
+        SELECT name FROM heroes WHERE id = {}
+        """.format(item[0])
+        hero2_name = execute_query(query)[0][0]
+        query = """
+        SELECT name FROM relationship_types WHERE id = {}
+        """.format(item[1])
+        relationship_type = execute_query(query)[0][0]
+        match relationship_type:
+            case "Friend":
+                friends.append(hero2_name)
+            case "Enemy":
+                enemies.append(hero2_name)
+    
+    return [", ".join(friends), ", ".join(enemies)]
 
 
 action = input("What would you like to do? ").lower()
+# Create
+# Read (Y)
+# Update
+# Delete
 match action:
     case "view hero":
         view_hero()
